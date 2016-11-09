c70835780f023cd6d90c70e334d359134cc23e0e
ZZR-china <2467254599@qq.com>
2016-11-09 12:55:09 +0800

    change scene to schema id

--- app/controller/testwc.js
+++ app/controller/testwc.js
@@ -10,34 +10,16 @@ router.route('/testwc')
 
 router.use('/testwc', wechat('testwc', (req, res, next) => {
     // 微信输入信息都在req.weixin上
-    var scence = [123,222];
     var message = req.weixin;
     var openid = message.FromUserName,
-        scence_id = Number(message.EventKey);
-        // res.reply(openid);
+        scence = Number(message.EventKey);
+    // res.reply(openid);
     console.log(openid);
-    console.log(scence_id);
-    var keyIndex = scence.indexOf(scence_id);
-    console.log('keyIndex',keyIndex);
-    switch(keyIndex){
-      case 0:
-      {
-        res.reply({
-            content: '用户的openid为'+openid+'场景是'+scence_id,
-            type: 'text'
-        });
-      }
-      break;
-      default:
-      {
-        res.reply({
-          content: '没有这个场景!'+scence_id,
-          type: 'text'
-        });
-      }
-      break;
-    }
-
+    console.log(scence);
+    res.reply({
+        content: '用户的openid为' + openid + '场景是' + scence,
+        type: 'text'
+    });
 }));
 
 
